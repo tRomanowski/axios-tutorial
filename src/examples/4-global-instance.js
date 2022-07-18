fad239de317ed4459952d6891f30cb194772f054
Tim Romanowski <timromanowski15@gmail.com>
2022-07-18 21:10:46 +0200

    Finish global and custon instance

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,11 +1,14 @@
 import { useEffect } from 'react';
-
+import axios from 'axios';
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const response1 = await axios(productsUrl);
+      const response2 = await axios(randomUserUrl);
+    } catch (error) {}
   };
 
   useEffect(() => {
--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/custom';
+import axios from 'axios';
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+    try {
+      const resp1 = await authFetch('/react-store-products');
+      const resp2 = await axios(randomUserUrl);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
