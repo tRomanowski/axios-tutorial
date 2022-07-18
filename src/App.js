904dbd90e3933a58d1779db2d52f5e03d8e00f87
Tim Romanowski <timromanowski15@gmail.com>
2022-07-18 20:31:35 +0200

    Finish first request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Title from './components/Title';
+import Setup from './examples/1-first-request';
+
 function App() {
   return (
     <main>
       <Title />
+      <Setup />
     </main>
   );
 }
